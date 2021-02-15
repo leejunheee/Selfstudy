a4f4bc5108b5124594224744ee1791d876ac7223
junhee <junhee@localhost>
2021-02-15 18:10:28 +0900

    2

--- java_tutorials/src/org/opentutorials/javatutorials/numberstring/Number.java
+++ java_tutorials/src/org/opentutorials/javatutorials/numberstring/Number.java
@@ -3,7 +3,7 @@ package org.opentutorials.javatutorials.numberstring;
 public class Number {
 
 	public static void main(String[] args) {
-		System.out.println('생');
+		System.out.println(1+1);
 	}
 
 }
