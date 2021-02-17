ca69d845502ce74227dc4d00282c2e5ad2c6c952
leejunheee <z6z6jun@naver.com>
2021-02-17 14:04:57 +0900

    2/17

---
+++ java_tutorials/src/org/opentutorials/javatutorials/datatype/IntDatatypeDemo.java
@@ -0,0 +1,17 @@
+package org.opentutorials.javatutorials.datatype;
+
+public class IntDatatypeDemo {
+
+	public static void main(String[] args) {
+		byte a;
+		a = 127;
+		
+		short b;
+		b = 9999;
+		
+		System.out.println(a+b);
+		
+			
+	}
+
+}
