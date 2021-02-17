c6170e00f419b26b1756be581431f6797681be27
leejunheee <z6z6jun@naver.com>
2021-02-17 18:18:32 +0900

    2/17

--- java_tutorials/src/org/opentutorials/javatutorials/operator/PrePostDemo.java
+++ java_tutorials/src/org/opentutorials/javatutorials/operator/PrePostDemo.java
@@ -3,8 +3,14 @@ package org.opentutorials.javatutorials.operator;
 public class PrePostDemo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int i = 3;
+		i++;
+		System.out.println(i);
+		++i;
+		System.out.println(i);
+		System.out.println(++i);
+		System.out.println(i++);
+		System.out.println(i);
 	}
 
 }
