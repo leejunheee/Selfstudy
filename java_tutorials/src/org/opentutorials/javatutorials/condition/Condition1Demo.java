90f907b876c7673c79c0b6b855792a04ffa469c0
leejunheee <z6z6jun@naver.com>
2021-02-17 18:56:52 +0900

    2/17

--- java_tutorials/src/org/opentutorials/javatutorials/condition/Condition1Demo.java
+++ java_tutorials/src/org/opentutorials/javatutorials/condition/Condition1Demo.java
@@ -3,8 +3,9 @@ package org.opentutorials.javatutorials.condition;
 public class Condition1Demo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		if(true) {
+			System.out.println("result : true");
+		}
 	}
 
 }
