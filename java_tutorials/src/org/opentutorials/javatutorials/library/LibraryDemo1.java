35cf18e7564281cf35ce8412da3f7c998eb7cd54
leejunheee <z6z6jun@naver.com>
2021-03-10 00:00:47 +0900

    3/9

---
+++ java_tutorials/src/org/opentutorials/javatutorials/library/LibraryDemo1.java
@@ -0,0 +1,10 @@
+package org.opentutorials.javatutorials.library;
+import java.lang.*; // 작성하지 않아도 자동으로 임포트 해줌.
+
+public class LibraryDemo1 {
+
+	public static void main(String[] args) {
+		System.out.println(1); // 자바 lang이라는 패키지안에 있는거임
+	}
+
+}
